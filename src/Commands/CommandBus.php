603b58e197b86ddeb7ddf0cbb38643b9819ac95b
Alexander Minin <a.s.minin@me.com>
2017-12-22 03:40:35 +0300

    add not command handler

--- src/Commands/CommandBus.php
+++ src/Commands/CommandBus.php
@@ -151,14 +151,31 @@ class CommandBus
     {
         $match = $this->parseCommand($message);
         if (!empty($match)) {
-            $command = $match[1];
+            $command   = $match[1];
             $action    = (!empty($match[2])) ? $match[2] : '';
-            $arguments = str_replace(["\n", "    ","   ","  "], [" "," "," "," "], trim($match[3]));
-            $arguments = explode(" ", $arguments);
+            $arguments = $this->parseArguments($match[3]);
             $this->execute($command, $action, $arguments, $update);
+        } elseif (array_key_exists('not_command', $this->commands)) {
+            // Use unnamed command handler for not command messages
+            $this->commands['not_command']->make($this->telegram, "", $this->parseArguments($message), $message);
         }
 
         return $update;
+    }
+
+    /**
+     * Parse a Arguments.
+     *
+     * @param $message
+     *
+     * @return array
+     */
+    public function parseArguments($message)
+    {
+        $arguments = str_replace(["\n", "    ","   ","  "], [" "," "," "," "], trim($message));
+        $arguments = explode(" ", $arguments);
+
+        return $arguments;
     }
 
     /**
